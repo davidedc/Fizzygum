b19f954608c11b2daa65d3ceaca2e3dd4f6bfbf2
Davide Della Casa <davidedc@gmail.com>
2012-10-21 02:56:57 +0100

    new documentation script, now there are three different ways to look at the docs: docco (literary programming i.e. code commentary), quick API reference and detailed API reference

--- make_docs_please.sh
+++ make_docs_please.sh
@@ -1,3 +1,12 @@
-docco src/*.coffee
 rake
-docco build/morphee-coffee.coffee
+
+# unfortunately the -o option of docco doesn't work
+cd docs
+docco ../src/*.coffee
+docco ../build/morphee-coffee.coffee
+mv ./docs ./docco-code-commentary
+
+# go back to the root directory
+cd ../
+coffeedoc -o ./docs/quick-api-reference ./build/morphee-coffee.coffee
+codo -o ./docs/detailed-api-reference ./src/*.coffee
