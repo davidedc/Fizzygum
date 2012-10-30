08d3cb666ce2d878c53a8324b96d3400f9ec8e63
Davide Della Casa <davidedc@gmail.com>
2012-10-30 18:47:51 +0000

    adding print version of the overview doc

--- make_docs_please.sh
+++ make_docs_please.sh
@@ -11,6 +11,7 @@ cd ..
 cd buildSystem
 python generateOverviewDoc.py
 pandoc -s -S --toc -c pandoc.css ../docs/overview/filteredCommentsForReadableStandaloneDoc.md -o ../docs/overview/overview.html
+pandoc -s -S --toc -c pandocPrintVersion.css ../docs/overview/filteredCommentsForReadableStandaloneDoc.md -o ../docs/overview/overview-PrintVersion.html
 
 # go back to the root directory
 cd ../
