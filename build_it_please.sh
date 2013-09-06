ed48cc99001c46beb26ab43ed8553e1cfa754367
Davide Della Casa <davidedc@gmail.com>
2013-09-06 22:50:16 +0100

    removing the minification step from the main build file

--- build_it_please.sh
+++ build_it_please.sh
@@ -3,5 +3,7 @@ rake
 coffee -b -c -o build ./build/morphee-coffee.coffee 
 cp src/morphee-coffee.html build
 cp src/morphee-coffee-test-launcher.html build
-cd buildSystem
-python build.py --all --minified
+# add these back if you want the minified build.
+# The minified build makes all objects uninspectable
+#cd buildSystem
+#python build.py --all --minified
