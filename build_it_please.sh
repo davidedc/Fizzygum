8b229a3b5d072b16bc955eef8cf458bae5b47312
Davide Della Casa <davidedc@gmail.com>
2014-07-11 20:46:40 +0100

    added to html file: load FileSaver.js and jszip.min.js and updated build script so that the files from the submodules are put in the right place

--- build_it_please.sh
+++ build_it_please.sh
@@ -3,3 +3,6 @@ python ./buildSystem/build.py
 coffee -b -c -o build ./build/morphee-coffee.coffee 
 cp src/morphee-coffee.html build
 cp src/morphee-coffee-test-launcher.html build
+# copy the interesting js files from the submodules
+cp auxiliary\ files/FileSaver\ submodule/FileSaver.js build
+cp auxiliary\ files/JSZip\ submodule/dist/jszip.min.js build
