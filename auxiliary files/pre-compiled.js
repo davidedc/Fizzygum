a3cba11eb698982e6c51ad5acf702341714eb350
davidedc <davidedc@gmail.com>
2017-12-12 18:05:47 +0000

    renaming script to put precompiled code in the build

--- auxiliary files/pre-compiled.js
+++ auxiliary files/pre-compiled.js
@@ -3,6 +3,6 @@
 # to a page. A pre-compiled.zip file will
 # then download, which can be put in the
 # "right" place in the builds by running
-#     sh inject_pre-compiled.sh
+#     sh inject_pre-compiled_into_build.sh
 
 window.preCompiled = false;
