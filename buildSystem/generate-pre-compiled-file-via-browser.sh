1c288e79e56cc42e2f0c4aeafa44d9556ea2669b
Davide Della Casa <davidedc@gmail.com>
2020-09-10 22:26:54 +0100

    tweaks to build script

--- buildSystem/generate-pre-compiled-file-via-browser.sh
+++ buildSystem/generate-pre-compiled-file-via-browser.sh
@@ -11,7 +11,9 @@ TO_RUN=''
 
 if [[ "$HAYSTACK" == *"$NEEDLE"* ]]; then
    TO_RUN="$FIZZYGUM_CHROME_PATH_WINDOWS $FIZZYGUM_PAGE_PATH_WINDOWS$URL_PARAM"
+   echo "rm $DOWNLOADS_DIRECTORY/pre-compiled*.zip"
    eval "rm $DOWNLOADS_DIRECTORY/pre-compiled*.zip"
+   echo $TO_RUN
    eval $TO_RUN
    sleep 12
    unzip -o -d $BUILD_PATH/js/ $DOWNLOADS_DIRECTORY/pre-compiled.zip
