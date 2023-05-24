5c73f4fefabfb25a589f2ffc4636fd0efc4c8da3
Davide Della Casa <davidedc@gmail.com>
2023-05-24 09:34:00 +0100

    tweaks to script to generate simple manifest for webm files

--- buildSystem/generate-simple-JSON-manifest-for-webm-files.sh
+++ buildSystem/generate-simple-JSON-manifest-for-webm-files.sh
@@ -3,14 +3,21 @@
 # Create the privateVideosManifest.js file with the list of files
 echo "const privateVideos = {" > privateVideosManifest.js
 echo "    \"files\": [" >> privateVideosManifest.js
-for file in *.webm;
+all_files=$(ls -1 *.webm)
+last_file=$(echo "$all_files" | tail -n 1)
+#echo "> $all_files < "
+echo "> $last_file < "
+
+echo "$all_files" | while read -r file;
 do
-  if [ "$file" != "$(ls -1 *.webm | tail -1)" ]; then
-    echo "      \"$file\"," >> privateVideosManifest.js
-  else
-    # make sure that the last line does not have a comma
-    echo "      \"$file\"" >> privateVideosManifest.js
-  fi
+    # echo "> $file < "
+    if [ "$file" != "$last_file" ]; then
+      echo "      \"$file\"," >> privateVideosManifest.js
+    else
+      # make sure that the last line does not have a comma
+      echo "      \"$file\"" >> privateVideosManifest.js
+    fi
 done
+
 echo "    ]" >> privateVideosManifest.js
 echo "};" >> privateVideosManifest.js
