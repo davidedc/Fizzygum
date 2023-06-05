6d67ea923162d8c309c01330b675732e4d7cacba
Davide Della Casa <davidedc@gmail.com>
2023-06-05 22:03:52 +0100

    video scripts that recompress/resize now skip already-processed source files

--- video-related-scripts/compress-videos-to-mp4-reasonable-size.sh
+++ video-related-scripts/compress-videos-to-mp4-reasonable-size.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# some video files are just huge, re-encode them to something sensible
+# some video files are just huge, re-encode them to something sensible AND in .mp4 format (for Safari mobile)
 
 
 # Loop through all .webm files in the current directory
@@ -12,9 +12,14 @@ for file in *.webm; do
         # Create the output file name
         output_file="${filename}.mp4"
 
-        # Encode the video with some sensible settings
-        ffmpeg -i "$file" -movflags +faststart -c:v libx264 -profile:v high -bf 2 -g 30 -coder 1 -pix_fmt yuv420p -crf 28 "$output_file"
+        # Check if the output file already exists
+        if [[ ! -f "$output_file" ]]; then
+            # Encode the video with some sensible settings
+            ffmpeg -i "$file" -movflags +faststart -c:v libx264 -profile:v high -bf 2 -g 30 -coder 1 -pix_fmt yuv420p -crf 28 "$output_file"
 
-        echo "Converted $file to $output_file"
+            echo "Converted $file to $output_file"
+        else
+            echo "Skipping $file, $output_file already exists"
+        fi
     fi
 done
--- video-related-scripts/compress-videos-to-reasonable-size.sh
+++ video-related-scripts/compress-videos-to-reasonable-size.sh
@@ -12,9 +12,14 @@ for file in *.webm; do
         # Create the output file name
         output_file="${filename}-fullHD.webm"
 
-        # Encode the video with some sensible settings
-        ffmpeg -i "$file" -vf scale=1920:-1 -c:v libvpx-vp9 -crf 37 -g 240 -tile-columns 2 -threads 8 -filter:v fps=30 -b:v 0 "$output_file"
+        # Check if the output file already exists
+        if [[ ! -f "$output_file" ]]; then
+            # Encode the video with some sensible settings
+            ffmpeg -i "$file" -vf scale=1920:-1 -c:v libvpx-vp9 -crf 37 -g 240 -tile-columns 2 -threads 8 -filter:v fps=30 -b:v 0 "$output_file"
 
-        echo "Converted $file to $output_file"
+            echo "Converted $file to $output_file"
+        else
+            echo "Skipping $file, $output_file already exists"
+        fi
     fi
 done
